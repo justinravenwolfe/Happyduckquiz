fee4cab2b7df5cf11c9ed70cdade5c3dbee1aa44
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2023-11-19 12:24:23 -0500

    doing starter of javascript with question options

--- index.js
+++ index.js
@@ -2,8 +2,21 @@
 // within this java script im grabbing that button
 var submit_button = document.getElementById("start-button");
 var score = 0;
+//120 seconds total for the quiz
+var time = 120; 
+var question_index = 0; 
 //Question Data
-
+var question_list = ["In which borrow is Yankees stadium located?",
+"Which one of these players was never on the Yankees?",
+"What color scheme are the Yankees home team uniforms?",
+"How many World Series rings have the Yankees won?"];
+//The multiple choice answers corresponding to each question
+// we are using interger for one questions because it has has to do with numbers
+var choices = [["Brooklyn","Bronx", "Queens", "NYC"],
+["Player1", "Player2", "Player3", "Player4"],
+["White with Blue pin-stripes", "Yellow with pokka dots", "solid red", "green"],
+[20,30,27,6]];
+var correct_answers = ["Bronx", "Player2", "White with Blue pin-stripes", 27];
 //Functions
 //Load the current question/answers, move to the next question
 function play_quiz(){
