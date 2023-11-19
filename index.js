6bf25c01bb7eb9ce16229019f00700c817ea5bfb
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2023-11-19 11:51:48 -0500

    adding skeleton of java script

---
+++ index.js
@@ -0,0 +1,19 @@
+//Var<- type indicator, means variable
+// within this java script im grabbing that button
+var submit_button = document.getElementById("start-button");
+var score = 0;
+//Question Data
+
+//Functions
+//Load the current question/answers, move to the next question
+function play_quiz(){
+
+}
+//Return whether the user has given the correct answer or not
+function check_answer(){
+
+}
+
+//DOM<- Document Object Model, In HTML ever item is a in object<- data/behavior. Using DOM
+//javascript can change items in the HTML
+// example of dom manipulation is once a user clicks a button the button turns red. anything that changed default views is dom manipulation.
